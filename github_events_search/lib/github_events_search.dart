542eb32ddf2cbf1fa6534c141bd94bbed6bcccd2
jeovazero <jeovapereira2000@live.com>
2021-02-23 02:42:15 -0300

    chore(lib): export all github events types

--- github_events_search/lib/github_events_search.dart
+++ github_events_search/lib/github_events_search.dart
@@ -1,3 +1,7 @@
 export 'src/search_events.dart';
 export 'src/events_type.dart';
 export 'src/events/base.dart';
+export 'src/events/issue.dart';
+export 'src/events/pull_request.dart';
+export 'src/events/push.dart';
+export 'src/events/watch.dart';
